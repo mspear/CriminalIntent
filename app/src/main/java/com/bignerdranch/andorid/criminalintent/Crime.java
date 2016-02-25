eb2c5cc0af92acafd31aa439f50702316f9ad126
Michael Spear <mwspear@gmail.com>
2016-02-25 11:59:09 -0600

    FInished chpt 8

--- app/src/main/java/com/bignerdranch/andorid/criminalintent/Crime.java
+++ app/src/main/java/com/bignerdranch/andorid/criminalintent/Crime.java
@@ -1,5 +1,6 @@
 package com.bignerdranch.andorid.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -8,11 +9,29 @@ import java.util.UUID;
 public class Crime {
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
 
 
     public Crime() {
         // Generate unique identifier
         mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
     }
 
     public UUID getId() {
